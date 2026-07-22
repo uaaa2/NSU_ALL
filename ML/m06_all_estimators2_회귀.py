738ee37b05129d6336fe3a776f69b378106001d5
PHS <qwe040629@gmail.com>
2026-07-22 17:42:22 +0900

    m06_2: all_estimators 로 사이킷런 전체 회귀 모델 일괄 평가 (당뇨)

---
+++ ML/m06_all_estimators2_회귀.py
@@ -0,0 +1,50 @@
+import numpy as np
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import RobustScaler
+import warnings
+warnings.filterwarnings('ignore')
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.utils import all_estimators
+
+#1. 데이터 
+x, y = load_diabetes(return_X_y=True)
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x,y, random_state=123, test_size= 0.2 
+)
+sclaer = RobustScaler()
+x_train = sclaer.fit_transform(x_train)
+x_test = sclaer.transform(x_test)
+
+#2. 모델구성
+#model = RandomForestRegressor()
+all_models = all_estimators(type_filter='regressor')
+
+# print("all_models", all_models)
+# print("모델의 갯수:", len(all_models))
+
+max_scare = 0
+max_name = '박현서'
+
+for (name, algorithm) in all_models:
+    try: 
+        model = algorithm()
+        
+        #3. 훈련 
+        model.fit(x_train,y_train)
+        #4, 평가, 예측
+        results = model.score(x_test, y_test)
+        print(name,'의 정답률',results)
+        
+        if results > max_scare: #
+            max_scare = results 
+            max_name = name
+        
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+    except: 
+        print(name,'는(은) 에러!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+        
+print("="*100)
+print("최고모델",max_name,":",max_scare)
+print("="*100)
