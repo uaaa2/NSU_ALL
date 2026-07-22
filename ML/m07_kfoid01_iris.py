07974be6f1a5fdf757adeb7888c701cc8d86b7f3
PHS <qwe040629@gmail.com>
2026-07-22 17:42:22 +0900

    m07_1: KFold 교차검증과 cross_val_score - 붓꽃 데이터

---
+++ ML/m07_kfoid01_iris.py
@@ -0,0 +1,18 @@
+from sklearn.model_selection import KFold, cross_val_score
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+import numpy as np
+
+#1 데이터
+x,y = load_iris(return_X_y=True)
+
+kfold = KFold(n_splits=5,shuffle=True,random_state=123)
+model = DecisionTreeClassifier()
+
+#3 컴파일, 훈련
+#4 평가, 예측
+scores = cross_val_score(model, x, y, cv=kfold,n_jobs=-1)
+
+print('ACC', scores,
+    '\n cross val scor의 평균:', round(np.mean(scores), 4)
+    )
