0e89a6103f33c1caaa7e2e30dd079344fa7df087
PHS <qwe040629@gmail.com>
2026-07-22 15:18:49 +0900

    m03_1: XOR 문제 - 단층 퍼셉트론으로는 못 푸는 이유

---
+++ ML/m3_xor_1.py
@@ -0,0 +1,25 @@
+import numpy as np
+from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import Perceptron
+
+
+#1. 데이터
+x_data = np.array([[0,0], [0,1], [1,0], [1,1]])
+y_data = np.array([0, 1, 1, 0])
+
+#2. 모델
+model = Perceptron()
+
+#3. 훈련 
+model.fit(x_data,y_data)
+                                                     #역전파 계산이 필요 없기 때문에 epochs가 필요없음
+
+#4. 평가, 예측
+y_predict = model.predict(x_data)                    #훈련한 모델에 x_data를 넣음
+
+result = model.score(x_data, y_data)    
+print(result)                                        #1.0 accuracy지수
+
+acc = accuracy_score(y_data, y_predict)
+print(f"acc= {acc}")  
